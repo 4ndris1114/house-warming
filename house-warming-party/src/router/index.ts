06f0ad7714de2aad8d333a9f71129bd2851bb4aa
Katarína Brezániová <10407558@ucn.dk>
2024-02-04 20:36:16 +0100

    redirecting to playlist

--- house-warming-party/src/router/index.ts
+++ house-warming-party/src/router/index.ts
@@ -1,14 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Signup from '../views/Signup.vue'
+import PasswordGame from '@/views/PasswordGame.vue'
+import Playlist from '@/views/Playlist.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    }
+      path: '/signup',
+      name: 'signup',
+      component: Signup
+    },
+    {
+      path: '/password-game',
+      name: 'passwordGame',
+      component: PasswordGame
+    },
+    {
+      path: '/playlist',
+      name: 'playlist',
+      component: Playlist
+    },
   ]
 })
 
---
+++ house-warming-party/src/views/Playlist.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <button @click="redirectToPlaylist">Go to Playlist</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      redirectToPlaylist() {
+        const playlistURI = '62IsjIZlurUQjilgkL4eo2?si=b90bf73e10ab40de&pt=02e91f7ef9c010d9ab2bb1bec78ea023';
+        window.location.href = `https://open.spotify.com/playlist/${playlistURI}`;
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
