ade02a20ba6d4b622e814c760fc586623de4e62c
Katarína Brezániová <10407558@ucn.dk>
2024-02-09 17:43:43 +0100

    experimenting with design

--- house-warming-party/src/views/HomeView.vue
+++ house-warming-party/src/views/HomeView.vue
@@ -7,15 +7,15 @@
         <p v-else>The countdown has ended!</p>
       </div>
     
-    <div class="menu">
-      <RouterLink to="/guest-list" class="cool-button">Guest list</RouterLink>
-      <router-link to="/requirements" class="cool-button">Requirements</router-link>
-      <router-link to="/playlist" class="cool-button">Playlist</router-link>
-      <router-link to="/password-game" class="cool-button">Change password</router-link>
-      <router-link to="/map" class="cool-button">Address</router-link>
-      <!-- <router-link to="/your-information" class="cool-button">Your information</router-link> -->
+      <div class="menu">
+        <RouterLink to="/guest-list" class="cool-button">Guest list</RouterLink>
+        <router-link to="/requirements" class="cool-button">Requirements</router-link>
+        <router-link to="/playlist" class="cool-button">Playlist</router-link>
+        <router-link to="/password-game" class="cool-button">Change password</router-link>
+        <router-link to="/map" class="cool-button">Address</router-link>
+        <!-- <router-link to="/your-information" class="cool-button">Your information</router-link> -->
+      </div>
     </div>
-  </div>
   </header>
   <router-view />
 </template>
@@ -66,42 +66,37 @@ const remainingTimeString = computed(() => {
 
 <style scoped>
 .countdown-frame {
-  padding: 20px;
+  padding: 1vw;
   background-color: #004b6b;
-  border: 20px solid #BE3455;
-  border-radius: 30px;
-  margin-top: 20px;
+  border: 2vw solid #BE3455;
+  border-radius: 5vw;
   text-align: center;
-  /* Center the content horizontally */
 }
 
 .countdown {
-  margin-bottom: 20px;
+  margin-bottom: 2vw;
 }
 
 .menu {
   display: flex;
   flex-direction: column;
-  /* Stack buttons vertically */
   align-items: center;
-  /* Center buttons horizontally */
 }
 
 .menu a {
-  margin: 10px;
-  padding: 12px 24px;
+  margin: 1vw; /* Use 1vw for margin */
+  padding: 1vw 2vw; /* Use 1vw for vertical padding and 2vw for horizontal padding */
   /* Adjust button padding */
-  font-size: 16px;
+  font-size: 1.5vw; /* Use 1.5vw for font size */
   font-weight: 800;
   text-align: center;
   text-decoration: none;
   cursor: pointer;
-  border: 2px solid #BE3455;
-  /* Add black border */
+  border: 0.25vw solid #BE3455;
   color: #BE3455;
   background-color: #ffffff;
-  border-radius: 30px;
-  width: 140px;
+  border-radius: 5vw;
+  width:10vw; /* Set width to auto */
   transition: background-color 0.3s, color 0.3s, border-color 0.3s;
 }
 
