d7dff4bfe198f991cf82b5fc085c7fdadc18063b
4ndris1114 <10407531@ucn.dk>
2024-02-12 10:01:22 +0100

    container padding

--- house-warming-party/src/views/GuestList.vue
+++ house-warming-party/src/views/GuestList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="card-container">
+    <div class="card-container" :style="{ paddingTop: containerPadding }">
         <div v-if="guests.length === 0" class="no-guests-message">No guests available.</div>
         <div class="cards-wrapper" v-else>
             <GuestCard v-for="guest in guests" :key="guest.uid" :guest="guest" class="card"></GuestCard>
@@ -20,51 +20,54 @@ onMounted(() => {
 const guests = computed(() => {
     return guestsStore.guests;
 });
+
+// Computed property to calculate the container padding
+const containerPadding = computed(() => {
+    const numGuests = guestsStore.guests.length;
+    const basePadding = 15;
+    const minPadding = 3;
+    const maxPadding = 150;
+    return `max(${minPadding}rem, min(${maxPadding}rem, ${basePadding * (numGuests/3)}rem))`;
+});
 </script>
 
 <style scoped>
 .card-container {
-    position: relative;
     display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    padding-top: 3rem; /* Add padding to accommodate the first card */
-    gap: 1.25rem;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 3rem;
 }
 
 .cards-wrapper {
     display: flex;
     flex-wrap: wrap;
+    justify-content: center;
+    gap: 1.25rem;
 }
 
 .card {
-    flex: 1 0 10rem;
+    flex: 1 1 20rem;
+    max-width: 20rem;
     margin: 0.6rem;
 }
 
 /* Additional styles for mobile view */
-@media (max-width: 910px) {
-    .card-container {
-        flex-direction: column;
-        align-items: center;
+@media (max-width: 500px) {
+    .card-wrapper {
         padding-top: 50rem;
+        /* Adjust padding for smaller screens */
     }
 
     .card {
-        width: 100%;
-        max-width: 20rem;
-        margin-bottom: 1.25rem;
-    }
-
-    .card:not(:first-child) {
-        margin-top: 1rem;
+        flex: 1 1 calc(50% - 1.25rem);
+        /* Display two cards per row on smaller screens */
     }
 }
 
-.no-guests {
-  font-style: italic;
-  color: #888;
-  width: 100%;
-  text-align: center;
-}
-</style>
+.no-guests-message {
+    font-style: italic;
+    color: #888;
+    width: 100%;
+    text-align: center;
+}</style>
